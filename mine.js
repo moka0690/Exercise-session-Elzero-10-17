c067092a498d940d4108ca0584ae526ad5b8a644
moka0690 <madradz39@gmail.com>
2024-02-17 22:08:23 +0100

    solveSolving exercise 2

--- mine.js
+++ mine.js
@@ -8,9 +8,12 @@ console.log(typeof "NumberOne")
 console.log(typeof "NumberTwo")
 console.log( NumberOne + NumberTwo)
 console.log(typeof "NumberOne + NumberTwo")
+let elzero ={ innerHTML:"object" } ;
 
 
 
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object/
 
 
 
