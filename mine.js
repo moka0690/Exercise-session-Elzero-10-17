d47597ea647bb9def15df92a3e960b2fc8b72d07
moka0690 <madradz39@gmail.com>
2024-02-17 20:52:47 +0100

    solveSolving exercise 1

---
+++ mine.js
@@ -0,0 +1,16 @@
+
+var  NumberOne = 10;
+var NumberTwo = 20;
+
+console.log(NumberOne); 
+console.log(NumberTwo);
+console.log(typeof "NumberOne")
+console.log(typeof "NumberTwo")
+console.log( NumberOne + NumberTwo)
+console.log(typeof "NumberOne + NumberTwo")
+
+
+
+
+
+
