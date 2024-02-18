031f417df1ecf536129cb5cfcc01f92990d2e32b
moka0690 <madradz39@gmail.com>
2024-02-18 20:59:54 +0100

    solveSolving exercise 3

--- mine.js
+++ mine.js
@@ -8,12 +8,30 @@ console.log(typeof "NumberOne")
 console.log(typeof "NumberTwo")
 console.log( NumberOne + NumberTwo)
 console.log(typeof "NumberOne + NumberTwo")
+ 
+
+
+
 let elzero ={ innerHTML:"object" } ;
-
-
 
 console.log(elzero.innerHTML); // object
 console.log(typeof elzero); // object/
 
+let a = "`I'm In";
+let b = "\\";
+let k = "\\";
+
+let c =" Love";
+let d ="  \\ ";
+let f = '"""';
+let g = " ''' ";
+let h = "++ With ++";
+let o = '\"""\\"""'
+let l = '""JavaScript""``';
 
 
+console.log( `${a}
+${b} ${k}
+${c} ${b} ${f} ${g} 
+${o}
+${h} ${l}`)      
